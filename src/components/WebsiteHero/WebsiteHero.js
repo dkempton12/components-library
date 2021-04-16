0212150802fa9c322962c90532de7499a50377f5
Dan Kempton <kemptondan@gmail.com>
2021-04-16 17:54:47 -0400

    mostly styles and prop-types

--- src/components/WebsiteHero/WebsiteHero.js
+++ src/components/WebsiteHero/WebsiteHero.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./WebsiteHero.scss";
 
 function WebsiteHero({ children, bg_color, text_color }) {
@@ -9,5 +10,15 @@ function WebsiteHero({ children, bg_color, text_color }) {
 }
 
 const themeColors = ["white", "black", "blue", "gray"];
+
+WebsiteHero.propTypes = {
+  bg_color: PropTypes.oneOf(themeColors),
+  text_color: PropTypes.oneOf(themeColors),
+};
+
+WebsiteHero.defaultProps = {
+  bg_color: "blue",
+  text_color: "white",
+};
 
 export default WebsiteHero;
