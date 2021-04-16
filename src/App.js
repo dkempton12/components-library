3cfdf3ad93629d9f14b262beb70c565a9c90ac23
Dan Kempton <kemptondan@gmail.com>
2021-04-16 18:02:46 -0400

    Adding a Container component for layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import Container from "./components/Layout/Container/Container";
 import WebsiteHero from "./components/WebsiteHero/WebsiteHero";
 
 function App() {
   return (
     <div className="App">
       <WebsiteHero bg_color="blue" text_color="white" />
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </Container>
     </div>
   );
 }
