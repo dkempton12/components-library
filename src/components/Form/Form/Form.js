798b7cfb211f9db5033b15c2c6ea7be3c2b32e38
Dan Kempton <kemptondan@gmail.com>
2021-04-21 18:04:17 -0400

    adding form container and text input components

---
+++ src/components/Form/Form/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Input from "../Input/Input";
+
+function Form() {
+  return (
+    <form className="form">
+      <Input />
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/components/Form/Input/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input() {
+  return (
+    <div className="field">
+      <input type="text" />
+    </div>
+  );
+}
+
+export default Input;
