749ae34df21bfaeffc0ed6c3939f7df8d07b619b
Dan Kempton <kemptondan@gmail.com>
2021-04-28 14:12:20 -0400

    converting Container to a styled component

--- src/components/Layout/Container/Container.js
+++ src/components/Layout/Container/Container.js
@@ -1,8 +1,14 @@
-import "./Container.scss";
 import styled from "styled-components";
 
-function Container({ children }) {
-  return <div className="container">{children}</div>;
-}
+const Container = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+  padding: 60px 30px;
+  text-align: left;
+
+  @media (max-width: 768px) {
+    padding: 30px 15px;
+  }
+`;
 
 export default Container;
