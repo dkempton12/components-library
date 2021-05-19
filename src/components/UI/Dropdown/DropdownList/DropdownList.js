55412fcc521fdd2e96081f7731f508da59809f70
Dan Kempton <kemptondan@gmail.com>
2021-05-19 18:02:24 -0400

    adding DropdownList styles

--- src/components/UI/Dropdown/DropdownList/DropdownList.js
+++ src/components/UI/Dropdown/DropdownList/DropdownList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import DropdownItem from "../DropdownItem/DropdownItem";
 
+import styles from "./DropdownList.module.scss";
+
 function DropdownList({ data }) {
   return (
-    <div>
+    <div className={styles.dropdownlist}>
       {data.map(item => (
         <DropdownItem key={item.id} name={item.name} value={item.value} />
       ))}
