c0e53e06a157592ec45a638e7be526ef9e42d15e
Dan Kempton <kemptondan@gmail.com>
2021-05-20 10:43:38 -0400

    updating IDs

--- src/components/UI/Dropdown/dropdownData.js
+++ src/components/UI/Dropdown/dropdownData.js
@@ -1,14 +1,14 @@
 const dropdownData = [
-  { name: "Item 1", value: "item1", id: 1 + Math.random() },
-  { name: "Item 2", value: "item2", id: 1 + Math.random() },
-  { name: "Item 3", value: "item3", id: 1 + Math.random() },
-  { name: "Item 4", value: "item4", id: 1 + Math.random() },
-  { name: "Item 5", value: "item5", id: 1 + Math.random() },
-  { name: "Item 6", value: "item6", id: 1 + Math.random() },
-  { name: "Item 7", value: "item7", id: 1 + Math.random() },
-  { name: "Item 8", value: "item8", id: 1 + Math.random() },
-  { name: "Item 9", value: "item9", id: 1 + Math.random() },
-  { name: "Item 10", value: "item10", id: 1 + Math.random() },
+  { name: "Item 1", value: "item1", id: 1 },
+  { name: "Item 2", value: "item2", id: 2 },
+  { name: "Item 3", value: "item3", id: 3 },
+  { name: "Item 4", value: "item4", id: 4 },
+  { name: "Item 5", value: "item5", id: 5 },
+  { name: "Item 6", value: "item6", id: 6 },
+  { name: "Item 7", value: "item7", id: 7 },
+  { name: "Item 8", value: "item8", id: 8 },
+  { name: "Item 9", value: "item9", id: 9 },
+  { name: "Item 10", value: "item10", id: 10 },
 ];
 
 export default dropdownData;
