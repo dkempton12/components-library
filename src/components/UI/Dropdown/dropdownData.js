268224a3b408da2b17958ee743c0b921a4e24b3d
Dan Kempton <kemptondan@gmail.com>
2021-06-08 22:02:10 -0400

    updating data for Dropdown

--- src/components/UI/Dropdown/dropdownData.js
+++ src/components/UI/Dropdown/dropdownData.js
@@ -1,14 +1,14 @@
 const dropdownData = [
-  { name: "United States", value: "US", id: 1 },
-  { name: "Afghanistan", value: "AF", id: 2 },
-  { name: "Albania", value: "AL", id: 3 },
-  { name: "Algeria", value: "DZ", id: 4 },
-  { name: "Andorra", value: "AD", id: 5 },
-  { name: "Angola", value: "AO", id: 6 },
-  { name: "Antigua and Barbuda", value: "AG", id: 7 },
-  { name: "Argentina", value: "AR", id: 8 },
-  { name: "Armenia", value: "AM", id: 9 },
-  { name: "Aruba", value: "AW", id: 10 },
+  { name: "United States", abbrev: "US", id: 1 },
+  { name: "Afghanistan", abbrev: "AF", id: 2 },
+  { name: "Albania", abbrev: "AL", id: 3 },
+  { name: "Algeria", abbrev: "DZ", id: 4 },
+  { name: "Andorra", abbrev: "AD", id: 5 },
+  { name: "Angola", abbrev: "AO", id: 6 },
+  { name: "Antigua and Barbuda", abbrev: "AG", id: 7 },
+  { name: "Argentina", abbrev: "AR", id: 8 },
+  { name: "Armenia", abbrev: "AM", id: 9 },
+  { name: "Aruba", abbrev: "AW", id: 10 },
 ];
 
 export default dropdownData;
