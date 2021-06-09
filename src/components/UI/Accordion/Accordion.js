c79428811571c08fb3e0941f0b450f2eec64f8a7
Dan Kempton <kemptondan@gmail.com>
2021-06-09 18:50:20 -0400

    adding toggle state for Accordion

--- src/components/UI/Accordion/Accordion.js
+++ src/components/UI/Accordion/Accordion.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Accordion.module.css";
 
 function Accordion({ children, title }) {
+  const [displayAccordion, setDisplayAccordion] = useState(false);
+
+  const accordionToggleHandler = () => {
+    setDisplayAccordion(true);
+  };
+
+  const accordionToggleUI = displayAccordion ? (
+    <div className={classes.accordioncontent}>{children}</div>
+  ) : (
+    ""
+  );
+
   return (
-    <div className={classes.accordion}>
+    <div className={classes.accordion} onClick={accordionToggleHandler}>
       <div className={classes.accordiontitle}>
         <h4>{title}</h4>
       </div>
-      <div className={classes.accordioncontent}>{children}</div>
+      {accordionToggleUI}
     </div>
   );
 }
